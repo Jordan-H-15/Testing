dac983b4c198cb453b14f0be4161aae85c7b5e61
JordanHeyes <jordanheyes@oakadmins-MacBook-Air.local>
2024-04-26 10:46:55 +0100

    @@

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Welcome from "./Screens/welcome";
+import Login from "./Screens/login";
+import SignUp from "./Screens/signup";
+
+const stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator
+      initialRouteName="welcome">
+        <stack.Screen name="welcome" component={Welcome} options={{headerShown: false}}/>
+        <stack.Screen name="login" component={Login} options={{headerShown: false}}/>
+        <stack.Screen name="signup" component={SignUp} options={{headerShown: false}}/>
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Screens/welcome.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { LinearGradient } from "expo-linear-gradient";
+import { styled } from 'nativewind';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import COLORS from '../constrants/colours';
+
+export default function Welcome() {
+  return (
+    <View>
+      <LinearGradient>
+
+      </LinearGradient>
+    </View>
+  )
+}
